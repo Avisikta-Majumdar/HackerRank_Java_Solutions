ce8c57d55adfd7773a39c6a2861930fe4eb40682
avisikta19 <47344480+avisikta19@users.noreply.github.com>
2020-11-09 21:53:29 +0530

    Add files via upload

---
+++ Java_Stdin_and_Stdout_I.java
@@ -0,0 +1,15 @@
+package Java_Introduction;
+import java.util.*;
+public class Java_Stdin_and_Stdout_I {
+	public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a,b,c;
+            a = scan.nextInt();
+            System.out.println(a);
+            b = scan.nextInt();
+            System.out.println(b);
+            c = scan.nextInt();
+            System.out.println(c);
+    }
+
+}
