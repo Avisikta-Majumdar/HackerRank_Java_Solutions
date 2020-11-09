c04a842e198a274abc39a81cec207b61333b34cf
avisikta19 <47344480+avisikta19@users.noreply.github.com>
2020-11-09 21:48:47 +0530

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,11 @@
+package Java_Introduction;
+
+
+public class Solution {
+
+	    public static void main(String[] args) {
+	       System.out.println("Hello, World.");
+	       System.out.println("Hello, Java.");
+	    }
+	}
+
